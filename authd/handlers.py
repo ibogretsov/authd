8b8cb0b2d3af8c7549548fdd12848f8b222f1829
ilya <ilyabogrecov@gmail.com>
2017-08-16 22:33:45 +0300

    2_create_user: added time

--- authd/handlers.py
+++ authd/handlers.py
@@ -1,31 +1,38 @@
 import json
 import uuid
-import time
+import datetime
+import bcrypt
 
 import flask
-import sqlalchemy as sa
-from flask import request, make_response, abort, jsonify
-from voluptuous import Schema, Email, MultipleInvalid, All, Length
+import voluptuous as schema
 
 from authd import dataaccess
 from authd import models
 
 root = flask.Blueprint("root", __name__, url_prefix="")
-schema = Schema({"email": Email(), "password": All(str, Length(min=6))})
+USER_SCHEMA = schema.Schema({
+    schema.Required("email"): schema.Email(),
+    schema.Required("password"): schema.All(str, schema.Length(min=6))
+})
 
 
 @root.route("/users", methods=["POST"])
 def create_user():
-    data = json.loads(request.data)
+    data = json.loads(flask.request.data)
     try:
-        schema(data)
-    except MultipleInvalid as e:
-        abort(make_response(jsonify(message=str(e)), 400))
+        USER_SCHEMA(data)
+    except flask.MultipleInvalid as e:
+        flask.abort(
+            flask.make_response(flask.jsonify(message=str(e)), 400)
+        )
+    password = data["password"].encode()
+    hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
     with dataaccess.connect_db(flask.current_app.config["DSN"]) as session:
         user = models.User(
             user_id=str(uuid.uuid4()),
-            **data)
+            email=data["email"],
+            password=hash_password,
+            created=datetime.datetime.utcnow())
         session.add(user)
         session.commit()
-    return jsonify({"user_id": user.user_id}), 201
-# {"user": {"id": user.id}}
+    return flask.jsonify({"user_id": user.user_id}), 201
